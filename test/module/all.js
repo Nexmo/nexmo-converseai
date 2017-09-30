628a5bc7f932eab4bec083460cbe0997686bff60
leggetter <phil@leggetter.co.uk>
2017-09-30 15:22:13 +0100

    Commenting out test that can’t be run during deploy

--- test/module/all.js
+++ test/module/all.js
@@ -1,4 +1,5 @@
 // The purpose of this file is to run the API tests in sequence
 require('./module-send_sms-test');
 require('./module-send_verify_pin_code-test');
-require('./module-check_verify_pin_code-test');
+// Console output is not shown during `converse-cli deploy` so this test can only be run manually.
+//require('./module-check_verify_pin_code-test');
